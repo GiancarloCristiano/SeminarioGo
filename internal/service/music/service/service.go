4a1a2e9023f68d51ff2c0ac7774f1d0455361100
Giancarlo <gian-k@.live.com>
2020-12-02 22:40:19 -0300

    listo el crud

--- internal/service/music/service/service.go
+++ internal/service/music/service/service.go
@@ -17,8 +17,8 @@ type Service interface {
 	AddMusic(Music) (Music, error)
 	FindAll() ([]*Music, error)
 	FindByID(int) (*Music, error)
-	updateMusic(Music) (bool, error)
-	RemoveByID(int) (bool, error)
+	UpdateMusic(Music) (bool, error)
+	RemoveMusic(int) (bool, error)
 } 
 
 type service struct {
@@ -26,11 +26,12 @@ type service struct {
 	conf *config.Config
 }
 
-func New(db *sqlx.DB,c *config.Config) (Service, error){
+func New(db *sqlx.DB, c *config.Config) (Service, error){
 	return service{db,c}, nil
 }
 
-func (s service) AddMusic(m Music) (Music,error)  { 
+
+func (s service) AddMusic(m Music) (Music, error)  { 
 	query := "INSERT INTO music (name, artist, year) VALUES (?,?, ?)" 
 	res, err := s.db.Exec(query, m.Name, m.Artist, m.Year)
 	if err != nil{
@@ -39,4 +40,46 @@ func (s service) AddMusic(m Music) (Music,error)  {
 	m.ID,_ = res.LastInsertId()
 	fmt.Println(res.LastInsertId())
 	return m, nil
+}
+
+
+func (s service) FindAll() ([]*Music, error) {
+	var arr []*Music
+	query := "SELECT * FROM music"
+	if err := s.db.Select(&list,query); 
+	err != nil {
+		return nil, err
+	}
+		return arr, nil
+}
+
+
+func (s service) FindByID(ID int) (*Music, error) {
+	var music Music
+	query := "SELECT * FROM music WHERE ID = ?"
+	if err := s.db.Get(&music, query, ID); 
+	err != nil {
+		return nil, err
+	}
+	return &music, nil
+}
+
+
+func (s service) UpdateMusic(m Music) (bool,error)  { 
+	query := "UPDATE music SET Name = ?, Artist = ?, Year = ?, WHERE ID = ? " 
+	_, err := s.db.Exec(query, m.Name, m.Artist, m.Year, m.ID)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+
+func (s service) RemoveMusic(ID int) (bool,error)   {
+	query := "DELETE FROM music WHERE ID = ?"
+	_,err := s.db.Exec(query, ID) 
+	if err != nil {
+		return false, err
+	}
+	return true, nil
 }
